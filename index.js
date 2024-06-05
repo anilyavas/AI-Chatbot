133b9af5bbcf22f233c78927f6566b28791f2659
anilyavas <anil.yavas99@gmail.com>
2024-06-05 19:22:27 +0300

    openai

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 import fm from 'front-matter';
+import OpenAI from 'openai';
+
+const openai = new OpenAI();
 
 // on github:  https://raw.githubusercontent.com/expo/expo/main/docs/pages/get-started/start-developing.mdx
 // public page: https://docs.expo.dev/get-started/start-developing
@@ -15,6 +18,11 @@ const parseExpoDocs = async (slug) => {
 
 const handleDocs = async (slug) => {
   const data = parseExpoDocs(slug);
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
+    model: 'gpt-3.5-turbo',
+  });
+  console.log(completion.choices[0]);
   // generate vector
 };
 
